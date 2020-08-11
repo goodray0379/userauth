022c019d05c470e8796414c0b660d97652e7334a
heoyoungmin <goodray0379@gmail.com>
2020-08-11 16:02:38 +0900

    swagger setting

---
+++ src/main/java/com/devmin/userauth/config/WebMvcConfig.java
@@ -0,0 +1,32 @@
+package com.devmin.userauth.config;
+
+import com.devmin.userauth.component.JwtTokenProvider;
+import com.devmin.userauth.filter.JwtAuthenticationFilter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    private final AuthorizeArgumentResolver authorizeArgumentResolver;
+
+    //swagger Meta 정보 생성
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/")
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/")
+    }
+}
